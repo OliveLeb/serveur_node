89b1e11253af9a1f8e791205f2d736e968ac3483
OliveLeb <lebel.olivier@wanadoo.fr>
2020-03-27 21:13:21 +0100

    model param obj

--- model/modelParametre.js
+++ model/modelParametre.js
@@ -2,39 +2,40 @@ const mongoose = require('mongoose');
 const Joi = require('@hapi/joi');
 
 const schemaParam = mongoose.Schema({
-  information: [Object]
+  information: Object
 });
 
 const Param = mongoose.model('parametre', schemaParam);
 const schema = Joi.object({
-  information: Joi.array()
-    .items(
-      Joi.object()
-        .keys({
-          nbItem: Joi.number
-            .min(1)
-            .max(50)
-            .required(),
-          baliseSEO: Joi.string
-            .min(10)
-            .max(160)
-            .required(),
-          texteIntro: Joi.string
-            .min(20)
-            .max(300)
-            .required(),
-          titreAccueil: Joi.string
-            .min(5)
-            .max(50)
-            .required()
-        })
-        .min(4)
-        .max(4)
-        .required()
-    )
+  //Joi.array()
+  information:
+    // .items(
+    Joi.object()
+      .keys({
+        nbItem: Joi.number
+          .min(1)
+          .max(50)
+          .required(),
+        baliseSEO: Joi.string
+          .min(10)
+          .max(160)
+          .required(),
+        texteIntro: Joi.string
+          .min(20)
+          .max(300)
+          .required(),
+        titreAccueil: Joi.string
+          .min(5)
+          .max(50)
+          .required()
+      })
+      .min(1)
+      .max(10)
+      .required()
+  /*)
     .min(1)
     .max(10)
-    .required()
+    .required()*/
 });
 
 module.exports.schema = schema;
