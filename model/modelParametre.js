171d9e92546e27c8769878ec22eece670e8f81fb
OliveLeb <lebel.olivier@wanadoo.fr>
2020-03-27 20:33:18 +0100

    model param array obj

--- model/modelParametre.js
+++ model/modelParametre.js
@@ -1,13 +1,40 @@
-const mongoose = require("mongoose");
-const Joi = require("@hapi/joi");
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
 
 const schemaParam = mongoose.Schema({
-    information : String
+  information: [Object]
 });
 
-const Param = mongoose.model("parametre", schemaParam);
+const Param = mongoose.model('parametre', schemaParam);
 const schema = Joi.object({
-    information : Joi.string().min(3).max(255).required()
+  information: Joi.array()
+    .items(
+      Joi.object()
+        .keys({
+          nbItem: Joi.number
+            .min(1)
+            .max(50)
+            .required(),
+          baliseSEO: Joi.string
+            .min(10)
+            .max(160)
+            .required(),
+          texteIntro: Joi.string
+            .min(20)
+            .max(300)
+            .required(),
+          titreAccueil: Joi.string
+            .min(5)
+            .max(50)
+            .required()
+        })
+        .min(4)
+        .max(4)
+        .required()
+    )
+    .min(1)
+    .max(10)
+    .required()
 });
 
 module.exports.schema = schema;
